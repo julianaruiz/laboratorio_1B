ef2947c51608fc5bd9cd3234968b652553b77090
julianaruiz <juliana.ruizs@udea.edu.co>
2020-09-30 21:24:45 -0500

    correcto

--- main.cpp
+++ main.cpp
@@ -14,96 +14,32 @@ using namespace std;
 int main()
 {
     int devo,div_cincuentamil;
-    float div_50,div_1,div_2,div_5,div_mil,div_dosmil,div_cincomil,div_diezmil,div_veintemil,res;
+    float div_50,div_1,div_2,div_5,div_mil,div_dosmil,div_cincomil,div_diezmil,div_veintemil,res,con_diez,con_mil,con_1;
     cout << "Ingrese el monto a devolver" << endl;
     cin >> devo;
-    div_cincuentamil=devo/50000;
-    div_veintemil=(devo%50000)/20000;
-
-    div_cincomil=(devo%10000)/5000;
-    div_dosmil=(devo%5000)/2000;
-
-    div_5=(devo%1000)/500;
-    div_2=(devo%500)/200;
-
-    div_50=(devo%100)/50;
+    div_cincuentamil=devo/50000;  //genera la cantidad de billetes de 50.000 a devolver.
+    div_veintemil=(devo%50000)/20000; //genera la cantidad de billetes de 20.000 a devolver.
+    con_diez=devo/10000;
+    if(con_diez==8 || con_diez==6 || con_diez==3 || con_diez==1)  //Devuelve la cantidad de billetes de 10.000 según el entero dado.
+        div_diezmil=1;
+    else
+        div_diezmil=0;
+    div_cincomil=(devo%10000)/5000; //genera la cantidad de billetes de 5.000 a devolver.
+    div_dosmil=(devo%5000)/2000;   //genera la cantidad de billetes de 2.000 a devolver.
+    con_mil=(devo%10000)/1000;   //genera la cantidad de billetes de 1.000 a devolver.
+    if(con_mil==8 || con_mil==6 || con_mil==3 || con_mil==1) //Devuelve la cantidad de billetes de 1.000 según el entero dado.
+        div_mil=1;
+    else
+        div_mil=0;
+    div_5=(devo%1000)/500;  //genera la cantidad de monedas de 500 a devolver.
+    div_2=(devo%500)/200;   //genera la cantidad de monedas de 200 a devolver.
+    con_1=(devo%1000)/100;
+    if(con_1==8 || con_1==6 || con_1==3 || con_1==1)  //Devuelve la cantidad de monedas de 100 según el entero dado.
+       div_1=1;
+    else
+        div_1=0;
+    div_50=(devo%100)/50;  //genera la cantidad de monedas de 50 a devolver.
     res=(devo%100)%50;
-
-    if(devo%200/100>=1 || devo%2000/1000>=1 || devo%20000/10000>=1){
-        if((devo%1000)/500==1 || (devo%10000)/5000==1 || (devo%100000)/50000==1){
-            div_1=0;
-            div_mil=0;
-            div_diezmil=0;
-        }
-        else{
-            div_1=1;
-            div_mil=1;
-            div_diezmil=1;
-        }
-    }
-    else{
-        if((devo%500)/200>=1 || (devo%5000)/2000>=1 || (devo%50000)/20000>=1){
-            div_1=1;
-            div_mil=1;
-            div_diezmil=1;
-        }
-        else{
-            if((devo%200)/100>=1 || (devo%2000)/1000>=1 || (devo%20000)/10000>=1){
-                div_1=0;
-                div_mil=0;
-                div_diezmil=0;
-            }
-            else{
-                div_1=1;
-                div_mil=1;
-                div_diezmil=1;
-            }
-        }
-    }
-/*
-    if(devo%2000/1000>=1){
-        if((devo%10000)/5000==1)
-            div_mil=0;
-        else
-            div_mil=1;
-
-
-    }
-    else{
-        if((devo%5000)/2000>=1)
-            div_mil=1;
-        else{
-            if((devo%2000)/1000>=1)
-                div_mil=0;
-            else
-                div_mil=1;
-        }
-
-    }
-
-    if(devo%20000/10000>=1){
-        if((devo%100000)/50000==1)
-            div_diezmil=0;
-        else
-            div_diezmil=1;
-
-
-    }
-    else{
-        if((devo%50000)/20000>=1)
-            div_diezmil=1;
-        else{
-            if((devo%20000)/10000>=1)
-                div_diezmil=0;
-            else
-                div_diezmil=1;
-        }
-
-    }
-
-
-*/
-
 
     cout<<"50.000: "<<div_cincuentamil<<endl;
     cout<<"20.000: "<<div_veintemil<<endl;
@@ -115,6 +51,6 @@ int main()
     cout<<"200: "<<div_2<<endl;
     cout<<"100: "<<div_1<<endl;
     cout<<"50: "<<div_50<<endl;
-     cout<<"50: "<<res<<endl;
+     cout<<"residuo: "<<res<<endl;
     return 0;
 }
